31b95ab8821890f75e10a18a822a66fb32995275
goliney <serhii+github@holinei.com>
2026-07-29 12:25:08 -0400

    fix: dev build user service api endpoint

--- packages/experience/src/constants/env.ts
+++ packages/experience/src/constants/env.ts
@@ -8,4 +8,8 @@ export const isDevFeaturesEnabled =
  */
 export const userServiceEndpoint =
   process.env.USER_SERVICE_ENDPOINT ??
-  (process.env.NODE_ENV === 'production' ? 'https://user.api.zerve.ai' : 'http://localhost:3000');
+  (process.env.NODE_ENV === 'production'
+    ? window.origin.includes('zerve-dev')
+      ? 'https://user.api.zerve-dev.io' // Dev build (app.zerve-dev.io)
+      : 'https://user.api.zerve.ai' // Prod build (app.zerve.ai)
+    : 'http://localhost:8080'); // Local development (localhost:8080)
